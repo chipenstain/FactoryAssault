1b748d4b8c3963016541cc4ac6e78a7cd3844e57
chipenstain <chipenstaindev@gmail.com>
2022-01-26 04:33:36 +1000

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FactoryAssault
+{
+    public class PlayerController : MonoBehaviour
+    {
+       private CharacterController controller;
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# FactoryAssault
+
+>*test task
